4ed0a8ab951538d25827bd1763eeca7568182b63
Mennatullah Ibraheem <mennaibraheem2@gmail.com>
2023-05-24 16:06:50 +0300

    fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,22 +4,7 @@ export default function page() {
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
       <div className="m-2 space-y-2">
         <p>about this project </p>
-        <p>
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard a Latin professor at Hampden-Sydney
-          College in Virginia, looked up one of the more obscure Latin words,
-          consectetur, from a Lorem Ipsum passage, and going through the cites
-          of the word in classical literature, discovered the source. Lorem
-          Ipsum comes from sections 1.10.32 and 1.10.33 of The Extremes of Good
-          and Evil by Cicero, written in 45 BC. This book is a treatise on the
-          theory of ethics, very popular during the Renaissance. The first line
-          of Lorem Ipsum, "Lorem ipsum dolor sit", comes from a line in section
-          1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is
-          reproduced below for those interested. Sections 1.10.32 and 1.10.33
-          from by Cicero are also reproduced in their exact original form,
-          accompanied by English versions from the 1914 translation by.
-        </p>
+        <p>testing</p>
       </div>
     </div>
   );
